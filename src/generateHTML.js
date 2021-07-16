9a26ee891f8a9f19a94115a959eb0098f37e7ca1
Hare-Ball <robby.hare@gmail.com>
2021-07-15 22:00:11 -0500

    updtaes to html generator

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -17,9 +17,13 @@ function generateHTML(employeeTeam){
             </head>
             <body>
                 <div class="jumbotron text-center">
-                    <h1>Team Member Profiles</h1>
+                    <h1 class="h-100 display-5 fw-bold">Team Member Profiles</h1>
                 </div>
+
+                <div class="d-grid gap-2">
                 ${employeeCards}
+                </div>
+
             </body>
             </html>`;
 }
@@ -28,12 +32,12 @@ function generateCards(employee){
     switch(employee.getRole()) {
         case "Manager":
             return `<section>
-                    <div class="col-sm-4" style= "width: 18rem; background-color: grey;">
-                    <h5 class="card-header">${employee.getRole()} &#128681;: ${employee.getName()}</h5>
+                    <div class="mx-auto mb-3" style= "width: 25rem; background-color: grey;">
+                    <h5 class="card-header">&#128081; ${employee.getRole()}: ${employee.getName()}</h5>
                     <ul>
-                        <li> ID &#128681;: ${employee.getID()} </li>
-                        <li> Email Address &#128231; :${employee.getEmail()}</li>
-                        <li> Office &#127984;: ${employee.getOfficeNumber()} </li>
+                        <li>&#128681; ID: ${employee.getID()}</li>
+                        <li>&#128231; Email Address: ${employee.getEmail()}</li>
+                        <li>&#127984; Office: ${employee.getOfficeNumber()}</li>
                     </ul>
                     </div>
                     </section>`
@@ -41,24 +45,24 @@ function generateCards(employee){
 
         case "Engineer":
             return `<section>
-                    <div class="col-sm-4" style= "width: 18rem; background-color: teal;">
-                    <h5 class="card-header">${employee.getRole()} &#128119;: ${employee.getName()}</h5>
+                    <div class="mx-auto mb-3" style= "width: 25rem; background-color: aquamarine;">
+                    <h5 class="card-header">&#128119; ${employee.getRole()}: ${employee.getName()}</h5>
                     <ul>
-                        <li> ID &#128681;: ${employee.getID()} </li>
-                        <li> Email Address &#128231; :${employee.getEmail()}</li>
-                        <li> GitHub &#128187;: ${employee.getGithub()} </li>
+                        <li>&#128681; ID: ${employee.getID()}</li>
+                        <li>&#128231; Email Address: ${employee.getEmail()}</li>
+                        <li>&#128187; GitHub: ${employee.getGithub()}</li>
                     </ul>
                     </section>`
                     break;
 
         case "Intern":
             return `<section
-                    <div class="col-sm-4" style= "width: 18rem; background-color: lavender;">
-                    <h5 class="card-header">${employee.getRole()} &#128296;: ${employee.getName()}</h5>
+                    <div class="mx-auto mb-3" style= "width: 25rem; background-color: lavender;">
+                    <h5 class="card-header">&#128296; ${employee.getRole()}: ${employee.getName()}</h5>
                     <ul>
-                        <li> ID &#128681;: ${employee.getID()} </li>
-                        <li> Email Address &#128231;: ${employee.getEmail()}</li>
-                        <li> School &#127891;: ${employee.getSchool()} </li>
+                        <li>&#128681; ID: ${employee.getID()}</li>
+                        <li>&#128231; Email Address: ${employee.getEmail()}</li>
+                        <li>&#127891; School: ${employee.getSchool()}</li>
                     </ul>
                     </section>`
                     break;
